6099305355088ca5dc1a78d0e3fbf03f6234a334
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-06 21:48:57 +0300

    lint

--- __test__/tap.test.js
+++ __test__/tap.test.js
@@ -1,9 +1,8 @@
-
 import { tap } from '../src/index.js'
 
 const myThing = 5
 
-tap('this is a child test', t => {
+tap('this is a child test', (t) => {
   t.pass('this passes')
   t.ok(myThing, 'this passes if truthy')
   t.equal(myThing, 5, 'this passes if the values are equal')
