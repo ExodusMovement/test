83f8d25857cf314781fe15af77437e4edbaa45cc
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-08 19:45:23 +0300

    hook jest/globals only when it exists

--- bin/jest.js
+++ bin/jest.js
@@ -3,4 +3,6 @@ import { mock } from 'node:test'
 
 Object.assign(globalThis, globals)
 
-if (mock.module) mock.module('@jest/globals', { defaultExport: globals, namedExports: globals })
+try {
+  if (mock.module) mock.module('@jest/globals', { defaultExport: globals, namedExports: globals })
+} catch {}
