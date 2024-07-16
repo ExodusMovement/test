f2320f736fc2c8277b471a02d97bf45cd23771da
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-16 22:40:10 +0300

    test: one more jest.mock testcase

---
+++ __test__/jest/jest.mock.require.5.test.cjs
@@ -0,0 +1,10 @@
+jest.mock('fs')
+
+const fs = require('fs')
+
+test('should do an opaque fs mock', () => {
+  const uuid = crypto.randomUUID()
+  expect(fs.existsSync).not.toHaveBeenCalledWith(uuid)
+  expect(fs.existsSync(uuid)).toBe(undefined)
+  expect(fs.existsSync).toHaveBeenCalledWith(uuid)
+})
