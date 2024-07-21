325e5cf8ab75a95b530514afeab61dc5752fc587
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-21 13:34:24 +0300

    make engine selection bundleable

--- src/engine.select.cjs
+++ src/engine.select.cjs
@@ -1,2 +1,5 @@
-const context = process.env.EXODUS_TEST_CONTEXT
-module.exports = context === 'pure' ? require('./engine.pure.cjs') : require('./engine.node.cjs')
+// Needs to be inlined for bundler to optimize it out correctly
+module.exports =
+  process.env.EXODUS_TEST_CONTEXT === 'pure'
+    ? require('./engine.pure.cjs')
+    : require('./engine.node.cjs')
