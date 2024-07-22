8fccabfb9e83e01ed594139b05c5f0ac6b024c5a
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-22 17:07:00 +0300

    minor test formatting

--- __test__/jest/jest.mock.import.1.test.js
+++ __test__/jest/jest.mock.import.1.test.js
@@ -1,15 +1,14 @@
 const describeModuleMocks = !jest.exodus || jest.exodus.features.esmMocks ? describe : describe.skip
 
-describeModuleMocks('Mocking non-builtin module import from esm', async () => {
+describeModuleMocks('Mocking non-builtin module import from esm', () => {
   jest.mock('c8', () => ({
     __esModule: true,
     default: { baz: 'BAR' },
     foo: 'some other foo',
   }))
 
-  const c8 = await import('c8')
-
-  test('should do a mock', () => {
+  test('should do a mock', async () => {
+    const c8 = await import('c8')
     expect(c8.__esModule).toBe(true)
     expect(c8.default.baz).toBe('BAR')
     expect(c8.foo).toBe('some other foo')
