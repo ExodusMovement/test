38813f408dc60fe1cda143ed1c4d7b7487c3a4b7
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-25 19:07:54 +0300

    better stub for throwing function

--- src/bundle-apis/empty/function-throw.cjs
+++ src/bundle-apis/empty/function-throw.cjs
@@ -1,3 +1,4 @@
-module.exports = () => {
+// Should not be an arrow function as might be used as a constructor
+module.exports = function () {
   throw new Error('module unsupported in bundled mode')
 }
--- src/bundle-apis/xmlhttprequest.cjs
+++ src/bundle-apis/xmlhttprequest.cjs
@@ -1,4 +1,5 @@
-const XMLHttpRequest = () => {
+// Should not be an arrow function as might be used as a constructor
+const XMLHttpRequest = function () {
   throw new Error('module unsupported in bundled form: xmlhttprequest, xmlhttprequest-ssl')
 }
 
