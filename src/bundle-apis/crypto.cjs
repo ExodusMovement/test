9cc71c273f1b90f1fae3d2b95c39c19559088d2e
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-27 00:38:25 +0300

    do not crash on global crypto undefined in bundle

--- src/bundle-apis/crypto.cjs
+++ src/bundle-apis/crypto.cjs
@@ -2,4 +2,4 @@ const cb = require('crypto-browserify')
 const webcrypto = globalThis.crypto
 const randomUUID = () => webcrypto.randomUUID()
 const getRandomValues = (array) => webcrypto.getRandomValues(array)
-module.exports = { ...cb, webcrypto, subtle: webcrypto.subtle, randomUUID, getRandomValues }
+module.exports = { ...cb, webcrypto, subtle: webcrypto?.subtle, randomUUID, getRandomValues }
