acd5ce47661d941dbb05b1da8f85164b34f3e82d
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-27 07:10:35 +0300

    we also support tape-promise API

---
+++ __test__/tape-promise.test.js
@@ -0,0 +1,42 @@
+import test from '@exodus/test/tape'
+
+// From https://www.npmjs.com/package/tape-promise
+// We don't allow using promises that are not async functions though
+
+// example function that returns a Promise
+// it could also be an async function
+function delay(time) {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve()
+    }, time)
+  })
+}
+
+test.skip('ensure promises works', function (t) {
+  return delay(100).then(function () {
+    t.true(true)
+  })
+})
+
+// NOTICE 'async'?
+test('ensure async works', async function (t) {
+  await delay(100)
+  t.true(true)
+  t.end() // not really necessary
+})
+
+test('ensure that regular test functions still work', function (t) {
+  t.true(true)
+  t.end()
+})
+
+const asyncFunction = async () => {
+  throw new Error('this function rejects')
+}
+
+test('reject and doesNotReject example', async (t) => {
+  await t.rejects(asyncFunction)
+  await t.rejects(asyncFunction())
+  await t.doesNotReject(Promise.resolve())
+})
