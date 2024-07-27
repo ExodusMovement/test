6a6bd4be3689400c47e9a58019c9e3642acf3fe3
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-27 07:56:39 +0300

    Node.js v18 fix

--- src/bundle-apis/node-buffer.cjs
+++ src/bundle-apis/node-buffer.cjs
@@ -1,2 +1,3 @@
 // For node.js Buffers to have the same ref / be fast
+const { Buffer, Blob, File } = globalThis
 module.exports = { Buffer, Blob, File, INSPECT_MAX_BYTES: 50, kMaxLength: 2 ** 31 - 1 }
