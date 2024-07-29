ca42bb8f3cc605acab19023e5a096d920b1ba120
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-29 17:15:38 +0300

    update amaro to 0.0.5

--- bin/typescript.loader.js
+++ bin/typescript.loader.js
@@ -1,17 +1,13 @@
-import { dirname, resolve as pathResolve } from 'node:path'
-import { createRequire } from 'node:module'
 import { readFile } from 'node:fs/promises'
+import { transformSync } from 'amaro'
 
-const require = createRequire(import.meta.url)
-const amaroDir = dirname(require.resolve('amaro/package.json'))
-const amaro = await import(pathResolve(amaroDir, 'dist/index.js'))
 const extensionsRegex = /\.ts$|\.mts$/
 
 export async function load(url, context, nextLoad) {
   if (extensionsRegex.test(url) && !url.includes('/node_modules/')) {
     const sourceBuf = await readFile(new URL(url))
     const source = sourceBuf.toString('utf8')
-    const transformed = amaro.transformSync(source, { isModule: true }).code
+    const { code: transformed } = transformSync(source, { isModule: true })
     const transformedBuf = Buffer.from(transformed)
     if (sourceBuf.length !== transformed.length) throw new Error('length mismatch')
     // eslint-disable-next-line unicorn/no-for-loop
