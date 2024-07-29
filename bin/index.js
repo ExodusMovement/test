166e7b1563280d4b1b5e33b7502fa6f87d146719
Nikita Skovoroda <chalkerx@gmail.com>
2024-07-29 15:36:40 +0300

    add json-summary coverage reporter to c8 --coverage

--- bin/index.js
+++ bin/index.js
@@ -353,7 +353,7 @@ if (options.coverage) {
     args.unshift(options.binary)
     options.binary = c8
     // perhaps use text-summary ?
-    args.unshift('-r', 'text', '-r', 'html', '-r', 'lcov')
+    args.unshift('-r', 'text', '-r', 'html', '-r', 'lcov', '-r', 'json-summary')
   } else {
     throw new Error(`Unknown coverage engine: ${JSON.stringify(options.coverageEngine)}`)
   }
