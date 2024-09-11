860f4ef06c926ad70c92df5e9e12112b921b73e2
Nikita Skovoroda <chalkerx@gmail.com>
2024-09-11 13:32:46 +0400

    test: module mocks are unavailable in inband tests

--- tests/inband/1.inband.test.js
+++ tests/inband/1.inband.test.js
@@ -7,3 +7,8 @@ test('inband 1', () => {
 test('inband 1 test 2', () => {
   expect(true).toBe(true)
 })
+
+test('module mocks are unavailable in inband tests', () => {
+  expect(jest.mock).toBe(undefined)
+  expect(jest.doMock).toBe(undefined)
+})
