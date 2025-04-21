3b38d11bbc3661d7dfddeaf6246a4634a4f1477d
Nikita Skovoroda <chalkerx@gmail.com>
2025-04-21 19:36:29 +0400

    feat: also look for bundle fixtures in 'vectors' and 'wycheproof' dirs

--- bundler/bundle.js
+++ bundler/bundle.js
@@ -212,7 +212,7 @@ export const build = async (...files) => {
   const fsFilesContents = new Map()
   const fsFilesDirs = new Map()
   const cwd = process.cwd()
-  const fixturesRegex = /(fixtures|samples)/u
+  const fixturesRegex = /(fixtures|samples|vectors|wycheproof)/u
   const aggressiveExtensions = /\.(json|txt|hex|wasm)$/u // These are bundled when just used in path.join and by wildcard from fixtures/
   const fileAllowed = (f) =>
     f && f.startsWith(`${cwd}/`) && resolve(f) === f && /^[a-z0-9@_./-]+$/iu.test(relative(cwd, f))
