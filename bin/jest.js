2fe142798aaed174c848ba1b2313ccc480f2f811
Nikita Skovoroda <chalkerx@gmail.com>
2025-04-24 04:44:46 +0400

    chore: don't expose global should in --jest

--- bin/jest.js
+++ bin/jest.js
@@ -1,4 +1,4 @@
 const { loadJestConfig, installJestEnvironment } = await import('../src/jest.config.js')
 await loadJestConfig()
-const jestGlobals = await import('../src/jest.js')
+const { should, ...jestGlobals } = await import('../src/jest.js') // eslint-disable-line @typescript-eslint/no-unused-vars
 await installJestEnvironment(jestGlobals)
