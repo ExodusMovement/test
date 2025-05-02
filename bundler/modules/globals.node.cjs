fc89a0cc0b6b0520feae87c038eb8098d040b3ef
Nikita Skovoroda <chalkerx@gmail.com>
2025-05-02 11:42:18 +0400

    fix: load global crypto on old Node.js bundles

--- bundler/modules/globals.node.cjs
+++ bundler/modules/globals.node.cjs
@@ -1,2 +1,8 @@
 // A slimmed-down version of globals.cjs specifically for Node.js bundle
 Object.assign(process, { argv: process.argv })
+
+if (!globalThis.crypto) {
+  // Old Node.js, we polyfill it as our bundler polyfills crypto module using webcrypto RNG
+  const r = require // prevent embed
+  globalThis.crypto = r('node:crypto').webcrypto
+}
