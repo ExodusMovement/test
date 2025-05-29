65642d7776795d5a11246d9bbb1fd380d1706398
Nikita Skovoroda <chalkerx@gmail.com>
2025-05-29 11:38:48 +0400

    feat: less opinionated --babel option

--- loaders/babel.cjs
+++ loaders/babel.cjs
@@ -2,7 +2,5 @@ const register = require('@babel/register')
 
 register({
   compact: false,
-  babelrc: false,
-  plugins: ['@babel/plugin-transform-modules-commonjs'],
   ignore: [], // do not ignore node_modules
 })
