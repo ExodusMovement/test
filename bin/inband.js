2c7190db0489931c21490adb24fb6e7b5b3ba095
Nikita Skovoroda <chalkerx@gmail.com>
2025-06-10 05:54:51 +0400

    fix: import file urls in inband runner

--- bin/inband.js
+++ bin/inband.js
@@ -1,4 +1,5 @@
 import { resolve } from 'node:path'
+import { pathToFileURL } from 'node:url'
 import { describe, after } from '../src/engine.js'
 
 const files = JSON.parse(process.env.EXODUS_TEST_INBAND)
@@ -6,7 +7,7 @@ if (!Array.isArray(files)) throw new Error('Unexpected')
 
 for (const file of files.sort()) {
   await describe(`EXODUS_TEST_INBAND:${file}`, async () => {
-    await import(resolve(file))
+    await import(pathToFileURL(resolve(file)))
   })
 }
 
