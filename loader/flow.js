96c5d64bf10c4b6e259d3659ccf31b11d68e3c1a
Nikita Skovoroda <chalkerx@gmail.com>
2025-06-12 17:58:10 +0400

    feat: also transform react-native code with esbuild under --flow

--- loader/flow.js
+++ loader/flow.js
@@ -1,15 +1,23 @@
 import { registerHooks } from 'node:module' // 22.15+
 import { readFileSync } from 'node:fs'
 import flowRemoveTypes from '@exodus/test-bundler/flow-remove-types'
+import { transformSync } from '@exodus/test-bundler/esbuild'
 
 const { URL } = globalThis
+
+const esbuildOptions = { platform: 'node', loader: 'jsx', format: 'cjs' }
 
 function load(url, context, nextLoad) {
   if (url.startsWith('file://')) {
     const source = readFileSync(new URL(url), 'utf8')
     if (source.includes('@flow')) {
-      const stripped = flowRemoveTypes(source, { pretty: true }).toString()
-      return { format: context.format || 'commonjs', source: stripped, shortCircuit: true }
+      let transformed = flowRemoveTypes(source, { pretty: true }).toString()
+      // TODO: perhaps transform more code?
+      if (url.includes('/node_modules/react-native/')) {
+        transformed = transformSync(transformed, esbuildOptions).code
+      }
+
+      return { format: context.format || 'commonjs', source: transformed, shortCircuit: true }
     }
   }
 
