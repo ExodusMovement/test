c6d786432963df678d39053ec5fe8adaaa372d2b
Nikita Skovoroda <chalkerx@gmail.com>
2025-06-14 12:05:02 +0400

    feat: disable followSymLinks in test globbing

--- src/glob.cjs
+++ src/glob.cjs
@@ -3,9 +3,10 @@ const fs = require('node:fs/promises')
 async function glob(patterns, { exclude, cwd }) {
   try {
     const fastGlob = require('fast-glob')
-    return fastGlob(patterns, { ignore: exclude, cwd })
+    return fastGlob(patterns, { ignore: exclude, cwd, followSymbolicLinks: false })
   } catch {}
 
+  // Experimental, but works
   return Array.fromAsync(fs.glob(patterns, { exclude, cwd }))
 }
 
