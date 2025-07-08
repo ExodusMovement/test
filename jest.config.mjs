697b9dfca2a3abb95c5909bcaeebd9abab4fb29f
Nikita Skovoroda <chalkerx@gmail.com>
2025-07-08 23:03:06 +0400

    test: setup-files is a jest feature

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,10 +1,10 @@
 const config = {
   testMatch: [`**/?(*.)+(spec|test).?([cm])[jt]s?(x)`], // From bin/index.js defaults
   setupFiles: [
-    '<rootDir>/tests/setup.cjs',
-    '<rootDir>/tests/setup-files/setup.cjs',
-    '<rootDir>/tests/setup-files/setup.mjs',
-    '<rootDir>/tests/setup-files/setup.js',
+    '<rootDir>/tests/jest/setup.cjs',
+    '<rootDir>/tests/jest/setup-files/setup.cjs',
+    '<rootDir>/tests/jest/setup-files/setup.mjs',
+    '<rootDir>/tests/jest/setup-files/setup.js',
   ],
 }
 
